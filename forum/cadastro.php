a3755b1de2c7f26e6a28ebc575dd87ac0776ef0b
rhyangabriel <rhyan.belem@aluno.senai.br>
2025-10-02 15:45:39 -0300

    02/10/2025

--- forum/cadastro.php
+++ forum/cadastro.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Cadastro de Usuário</h1>
+    
+</body>
+</html>
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $usuarios = simplexml_load_file("usuarios.xml");
