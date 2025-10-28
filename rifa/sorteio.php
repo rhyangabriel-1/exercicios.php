1c440e3f013cf4ce02c1f7e905d305d4817953ea
rhyangabriel <rhyan.belem@aluno.senai.br>
2025-10-28 13:29:32 -0300

    28/10/2025

--- rifa/sorteio.php
+++ rifa/sorteio.php
@@ -10,23 +10,48 @@
     <header>
         <form method ="POST" >
             <label for="nome">PREMIO:</label>
-            <input type="text" id="text" name="premio" required><br>
+            <input type="text" id="text" name="premio" required>
+            <label for="nome">Rifa minima:</label>
+            <input type="text" id="text" name="numminimo" required>
+            <label for="nome">Rifa maxima:</label>
+            <input type="text" id="text" name="nummaximo" required>
             <button type="submit">Sortear</button>
         </form>
     </header>
 </body>
 </html>
 <?php
-
-    $numero = rand(1, 1000);
-    echo "<h3>O premio sorteado foi:</h3> " . $_POST['premio'] . "<br><br>";
-    echo "<h3>A rifa premiada foi:</h3> $numero <br><br>";
-
-    date_default_timezone_set("America/Sao_Paulo");
-    echo "<h3>Data atual:<h3> " . date("d/m/Y") . "<br>";
-    echo "<h3>Hora atual:<h3> " . date("H:i:s") . "<br>";
-
-    echo "<h2> Os ganhadores são </h2>"
     
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $numminimo = $_POST['numminimo'];
+        $nummaximo = $_POST['nummaximo'];
+        if ($nummaximo > $numminimo) {
+            $numero = rand($numminimo, $nummaximo);
+        }
+        echo "<div class='sorteio'>";
+        echo "<h3>O premio sorteado foi:</h3> " . $_POST['premio'] . "<br><br>";
+        echo "<h3>A rifa premiada foi:</h3> $numero <br><br>";
+
+        date_default_timezone_set("America/Sao_Paulo");
+        echo "<h3>Data atual:</h3> " . date("d/m/Y") . "<br>";
+        echo "<h3>Hora atual:</h3> " . date("H:i:s") . "<br>";
+
+        echo "<h2> Os ganhadores são</h2> ";
+        
+        $arquivo = "ganhadores.txt";
+
+        // salva o novo ganhador no arquivo
+        $linha = $_POST['premio'] . " - Rifa nº " . $numero . " - " . date("d/m/Y H:i:s") . "\n";
+        file_put_contents($arquivo, $linha );
+
+        // lê e exibe todos os ganhadores
+        $ganhadores = file($arquivo);
+        echo "<ul>";
+        foreach ($ganhadores as $g) {
+            echo "<li>$g</li>";
+        }
+        echo "</ul>";
+        echo "</div>";
+    }
 
 ?>
